643cc54b2af8607365ccd6b9cd5696adab2ac36c
karokojnr <karoko.jnr99@gmail.com>
2023-07-24 11:55:18 +0300

    Server set up

--- server/lib/data/data_sources/database_data_source.dart
+++ server/lib/data/data_sources/database_data_source.dart
@@ -1,13 +1,12 @@
-// Data source
 
-// import 'package:proto/proto.dart';
+ import 'package:proto/proto.dart';
 
-// import '../db/data.dart';
+ import '../db/data.dart';
 
 class DatabaseDataSource {
-  // void addBook(Book book) => books.add(book);
+  void addBook(Book book) => books.add(book);
 
-  // void addManyBooks(List<Book> books) => books.addAll(books);
+  void addManyBooks(List<Book> books) => books.addAll(books);
 
-  // List<Book> getBooks() => books;
+  List<Book> getBooks() => books;
 }
--- server/lib/data/db/data.dart
+++ server/lib/data/db/data.dart
@@ -1,15 +1,19 @@
-// List of books
+import 'package:proto/proto.dart';
 
-// import 'package:proto/proto.dart';
-
-// final books = <Book>[
-//   Book()
-//     ..title = 'The Great Gatsby'
-//     ..author = 'F. Scott Fitzgerald',
-//   Book()
-//     ..title = 'The Grapes of Wrath'
-//     ..author = 'John Steinbeck',
-//   Book()
-//     ..title = 'Nineteen Eighty-Four'
-//     ..author = 'George Orwell',
-// ];
+final books = <Book>[
+  Book()
+    ..title = 'The Great Gatsby'
+    ..author = 'F. Scott Fitzgerald',
+  Book()
+    ..title = 'The Grapes of Wrath'
+    ..author = 'John Steinbeck',
+  Book()
+    ..title = 'Nineteen Eighty-Four'
+    ..author = 'George Orwell',
+  Book()
+    ..title = 'Ulysses'
+    ..author = 'James Joyce',
+  Book()
+    ..title = 'Lolita'
+    ..author = 'Vladimir Nabokov',
+];
